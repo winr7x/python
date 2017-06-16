fdce19159b4940b4619eda56ebb570a13f8e5958
Alex Shevelkin <ash@tvip.ru>
2017-06-16 13:17:42 +0300

    Add in-place string concatenation example

--- PEP8/programming_recommendations.py
+++ PEP8/programming_recommendations.py
@@ -1,1 +1,15 @@
 # Implementations of Python: PyPy, Jython, IronPython, Cython, Psyco
+
+
+def print_a_and_b(a, b):
+    print("a ==", a, "\tb ==", b)
+
+
+# In-place string concatenation
+a = "aaa"
+b = "bbb"
+print_a_and_b(a, b)
+a += b
+print_a_and_b(a, b)
+a = a + b
+print_a_and_b(a, b)

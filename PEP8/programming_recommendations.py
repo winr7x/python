73dd33a01665a061c979f14f9e0bce35d2b7646b
Alex Shevelkin <ash@tvip.ru>
2017-06-16 14:25:16 +0300

    Add ''.join() concatenation examples

--- PEP8/programming_recommendations.py
+++ PEP8/programming_recommendations.py
@@ -13,3 +13,14 @@ a += b
 print_a_and_b(a, b)
 a = a + b
 print_a_and_b(a, b)
+print('\n')
+
+# ''.join() concatenation
+iterable1 = ["display", "battery", "keyboard"]
+joined1 = " <--> ".join(iterable1)
+print('" <--> ".join(["display", "battery", "keyboard"]) == "' + joined1 + '"')
+print('\n')
+
+iterable2 = "xyz"
+joined2 = "+".join(iterable2)
+print('"+".join("xyz") == "' + joined2 + '"')

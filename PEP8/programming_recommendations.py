cb1c4f7576c4cbd411b1f0aeac075de56d8f3777
winr7x <winr7x@gmail.com>
2017-06-18 10:11:05 +0300

    PEP8: replace tabs with spaces

--- PEP8/programming_recommendations.py
+++ PEP8/programming_recommendations.py
@@ -1,73 +1,73 @@
 def item_1():
-	def print_a_and_b(a, b):
-	    print("a ==", a, "\tb ==", b)
+    def print_a_and_b(a, b):
+        print("a ==", a, "\tb ==", b)
 
-	# Implementations of Python: PyPy, Jython, IronPython, Cython, Psyco
+    # Implementations of Python: PyPy, Jython, IronPython, Cython, Psyco
 
-	def inplace_concatenation():
-		# In-place string concatenation
-		a = "aaa"
-		b = "bbb"
-		print_a_and_b(a, b)
-		a += b
-		print_a_and_b(a, b)
-		a = a + b
-		print_a_and_b(a, b)
-		print('\n')
+    def inplace_concatenation():
+        # In-place string concatenation
+        a = "aaa"
+        b = "bbb"
+        print_a_and_b(a, b)
+        a += b
+        print_a_and_b(a, b)
+        a = a + b
+        print_a_and_b(a, b)
+        print('\n')
 
-	def join_concatenation():
-		# ''.join() concatenation
-		a = ["display", "battery", "keyboard"]
-		b = " <--> ".join(a)
-		print('" <--> ".join(["display", "battery", "keyboard"]) == "' + b + '"')
-		print('\n')
+    def join_concatenation():
+        # ''.join() concatenation
+        a = ["display", "battery", "keyboard"]
+        b = " <--> ".join(a)
+        print('" <--> ".join(["display", "battery", "keyboard"]) == "' + b + '"')
+        print('\n')
 
-		c = "xyz"
-		d = "+".join(c)
-		print('"+".join("xyz") == "' + d + '"')
-		print('\n')
+        c = "xyz"
+        d = "+".join(c)
+        print('"+".join("xyz") == "' + d + '"')
+        print('\n')
 
-	inplace_concatenation()
-	join_concatenation()
+    inplace_concatenation()
+    join_concatenation()
 
 
 def item_2():
-	# Comparisons to singletons
-	a = 0
-	if a is not None:
-	    print('a is not None')
+    # Comparisons to singletons
+    a = 0
+    if a is not None:
+        print('a is not None')
 
-	# if a != None:             Bad
-	    # print('a != None')    Bad
+    # if a != None:             Bad
+        # print('a != None')    Bad
 
-	b = ()
-	print('b = () is',
-	      'true' if b else 'false',
-	      'in boolean context')
+    b = ()
+    print('b = () is',
+          'true' if b else 'false',
+          'in boolean context')
 
-	# my_var true in context of "is my_var initialized"
-	print('b = () is',
-	      'true' if b is not None else 'false',
-	      'in context "is initialized"')
+    # my_var true in context of "is my_var initialized"
+    print('b = () is',
+          'true' if b is not None else 'false',
+          'in context "is initialized"')
 
-	print('\n')
+    print('\n')
 
 
 def item_3():
-	# is not Vs. not .. is
-	class Cat:
-		pass
+    # is not Vs. not .. is
+    class Cat:
+        pass
 
-	cat1 = Cat()
-	cat2 = Cat()
+    cat1 = Cat()
+    cat2 = Cat()
 
-	if cat1 is not cat2:
-		print('cat1 is not cat2')
+    if cat1 is not cat2:
+        print('cat1 is not cat2')
 
-	#if not cat1 is cat2:				Bad
-		# print('cat1 not is cat2')		Bad
+    #if not cat1 is cat2:               Bad
+        # print('cat1 not is cat2')     Bad
 
-	print('\n')
+    print('\n')
 
 
 item_1()
